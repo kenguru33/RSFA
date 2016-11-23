451a37ba42b42a50d88d1d7ae90853af315c5b57
Bernt Anker <bernt.anker@me.com>
2016-11-23 01:19:36 +0100

    commit

--- src/app/yr-weather/yr-weather.component.ts
+++ src/app/yr-weather/yr-weather.component.ts
@@ -17,20 +17,12 @@ export class YrWeatherComponent implements OnInit {
   }
 
   ngOnInit() {
-    var yrno = require('yr.no-interface'),
-      dublin = {
-        lat: 53.3478,
-        lon: 6.2597
-      },
-      LOC_VER = 1.9;
-
-
-    yrno.locationforecast(dublin, LOC_VER, function (err, xml) {
-      if (err) {
-        // Something went wrong...
-      } else {
-        console.log(xml);
-      }
+    var client = require("metno-client");
+    client.getWeather({
+      params: {lat: 20.123, lon: 43.232},
+      request: {timeout: 1000}
+    }, function(error, report){
+      console.log(report);
     });
   }
 
