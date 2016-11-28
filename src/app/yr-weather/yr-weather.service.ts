349a44a5258ac2b2cf1b64dd6ec2e7bd8f3fd17e
Bernt Anker <bernt.anker@me.com>
2016-11-28 15:34:30 +0100

    commit

--- src/app/yr-weather/yr-weather.service.ts
+++ src/app/yr-weather/yr-weather.service.ts
@@ -5,7 +5,7 @@ import {Response, Http} from "@angular/http";
 @Injectable()
 export class YrWeatherService {
 
-  baseUrl = "http://localhost:8080/api/json2";
+  baseUrl = "http://81.0.146.52:8080/api/json2";
 
   constructor(private http: Http) {
   }
