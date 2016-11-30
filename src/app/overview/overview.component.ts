ab840e9761d5c2029ebe4f0b73f59fd4ef7e6126
Bernt Anker <bernt.anker@me.com>
2016-11-30 16:29:56 +0100

    Commit

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {VesselsService} from "../vessels/vessels.service";
+import {StationsService} from "../stations/stations.service";
+import {Station} from "../stations/models/station";
+import {Vessel} from "../vessels/models/vessel.model";
 
 @Component({
   selector: 'app-overview',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+  stations: Station[];
+  vessels: Vessel[];
+
+  constructor(private vesselsService: VesselsService, private stationsService: StationsService) { }
 
   ngOnInit() {
+    this.vesselsService.getVessels().subscribe(vessels => {
+      this.vessels = vessels;
+    });
+
+    this.stationsService.getStations().subscribe(stations => {
+      this.stations = stations;
+    });
   }
 
 }
